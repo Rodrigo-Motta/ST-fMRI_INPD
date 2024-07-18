3a434de0921c954cdbd267b8f73edf93c854b61b
rodrigo <rodrigodamottacc@gmail.com>
2024-07-18 16:03:06 +0100

    first commit

---
+++ data/preprocessing_nodetimeseries.py
@@ -0,0 +1,233 @@
+#
+# Created on Tue Jul 18 2024
+#
+# by Rodrigo M. Cabral-Carvalho
+
+# Adapted from Simon Dahan @SD3004
+#
+# Copyright (c) 2024 MeTrICS Lab
+#
+
+import os
+import argparse
+
+import numpy as np
+import pandas as pd
+
+from scipy import stats
+from sklearn.model_selection import StratifiedKFold
+
+
+def main(args):
+    """
+    Main function to preprocess timeseries data and compute adjacency matrices.
+
+    Args:
+        args: Parsed command-line arguments containing:
+            - filename: Path to the file with the list of subject IDs (.txt or .csv).
+            - ICA: Number of Independent Component Analysis (ICA) nodes.
+            - data_path: Root directory path where node timeseries data files are stored.
+            - output_folder: Path to the directory where results will be saved.
+
+    Variables:
+        ids: List of subject IDs loaded from the specified file.
+        nb_subject: Total number of subjects derived from the shape of `ids`.
+        data_path: Root directory path where node timeseries data files are stored.
+        T: Temporal length sequence.
+        data: Numpy array initialized to zeros with shape (nb_subject, 1, T, 333, 1) to store preprocessed timeseries data for each subject.
+        label: Numpy array initialized to zeros with shape (nb_subject,) to store labels corresponding to each subject.
+        non_used: List to keep track of subject IDs not used due to data issues.
+        subject_string: Formatted string representing the subject ID for constructing file paths.
+        filename: Specific filename for each subject's node timeseries data.
+        filepath: Full path to the node timeseries data file for each subject.
+        full_sequence: Timeseries data for a subject loaded from the file.
+        z_sequence: Z-score normalized timeseries data for a subject.
+        data_all: Concatenated array of z-score normalized timeseries data for all subjects.
+        A: Adjacency matrix computed based on the correlation of node timeseries data.
+        train_data: Subset of `data` used for training obtained from the StratifiedKFold split.
+        train_label: Subset of `label` corresponding to `train_data`.
+        test_data: Subset of `data` used for testing obtained from the StratifiedKFold split.
+        test_label: Subset of `label` corresponding to `test_data`.
+        idx: Index counter used to track the number of successfully processed subjects.
+        df: Pandas DataFrame containing the list of subject IDs not used.
+        skf: Instance of `StratifiedKFold` from `scikit-learn` used to split the data into training and testing sets.
+        fold: Counter to track the fold number in the StratifiedKFold split.
+    """
+     
+    ### loading subject list
+    print('')
+    print('#'*30)
+    print('Starting: preprocessing script')
+    print('#'*30)
+    print('')
+
+    if args.filename.find('txt') != -1:
+        ids = np.loadtxt(args.filename)
+        nb_subject = ids.shape[0]
+    elif args.filename.find('csv') != -1: 
+        ids = pd.read_csv(args.filename)
+        nb_subject = ids.shape[0]
+    else:
+        raise TypeError('filetype not implemented')
+
+
+    data_path = str(args.data_path)
+    
+    T = 100
+    data = np.zeros((nb_subject,1,T, 333,1))
+    label = np.zeros((nb_subject,))
+
+    print('')
+    print('Number of subjects: {}'.format(nb_subject))
+    print('Temporal length sequence: {}'.format(T))
+    print('')
+
+    ### loading node timeseries
+
+    print('')
+    print('#'*30)
+    print('Loading: node timeseries')
+    print('#'*30)
+    print('')
+
+    idx = 0
+    non_used = []
+    for i in range(nb_subject):
+
+        ## -----------
+        ids.gender.replace(1.0, 0, inplace=True)
+        ids.gender.replace(2.0, 1, inplace=True)
+        ## --------
+
+        # Original subjects files
+        #subject_string = format(int(ids[i,0]),'06d')
+
+        ## ---------
+        subject_string = str(int(ids.loc[i,'subject']))
+        ## --------
+        filename = '/Users/rodrigo/Documents/data/INPD/GordonConnBOLD/GordonConnBOLD-'+subject_string+'.txt'
+        filepath = os.path.join(str(data_path),filename)
+        if os.path.exists(filepath):
+            full_sequence = np.loadtxt(filepath)[4:]
+            if np.any(np.sum(full_sequence, axis=0) == np.nan):
+                print('contains nan')
+                continue
+            if np.any(np.sum(full_sequence, axis=0) == 0):
+                print('contains sum = 0')
+                continue
+            if full_sequence.shape[0] < T:
+                print('sequence too short :{}'.format(filepath))
+                continue
+
+            full_sequence = np.transpose(full_sequence[:T,:])
+
+            z_sequence = stats.zscore(full_sequence, axis=1)
+            
+            if idx ==0:
+                data_all = z_sequence
+            else:
+                data_all = np.concatenate((data_all, z_sequence), axis=1)
+            
+            data[idx,0,:,:,0] = np.transpose(z_sequence)
+            # Original subjects file
+            #label[idx] = ids[i,1]
+
+            ## --------
+            label[idx] = ids.loc[i, args.label]
+            ## --------
+
+            idx = idx + 1
+            if idx%100==0:
+                print('subject preprocessed: {}'.format(idx))
+            
+        else:
+            non_used.append(subject_string)
+    
+    ### keep only subjects
+    data = data[:idx,0,:,:,0]
+    label = label[:idx]
+
+    print('')
+    print('Number of subjects loaded: {}'.format(idx))
+    print('Data shape: {}'.format(data.shape))
+    print('')
+
+    df = pd.DataFrame(data=np.array(non_used))
+
+    try:
+        os.mkdir(args.output_folder)
+        print('Creating folder: {}'.format(args.output_folder))
+    except OSError:
+        print('folder already exist: {}'.format(args.output_folder))
+
+    df.to_csv(os.path.join(args.output_folder,'ids_not_used.csv'))
+
+    print('')
+    print('#'*30)
+    print('Computing: Adjacency Matrix')
+    print('#'*30)
+    print('')
+           
+    # compute adj matrix
+    n_regions = 333
+    A = np.zeros((n_regions, n_regions))
+    for i in range(n_regions):
+        for j in range(i, n_regions):
+            if i==j:
+                A[i][j] = 1
+            else:
+                A[i][j] = abs(np.corrcoef(data_all[i,:], data_all[j,:])[0][1]) # get value from corrcoef matrix
+                A[j][i] = A[i][j]
+    A = np.nan_to_num(A)
+    
+
+    print('')
+    print('saving adjacency matrix')
+    print('')
+
+    try:        
+        os.mkdir(os.path.join(args.output_folder,'node_timeseries'))  
+        print('Creating folder: {}'.format(os.path.join(args.output_folder,'node_timeseries')))
+    except OSError:
+        print('folder already exist: {}'.format(os.path.join(args.output_folder,'node_timeseries')))
+
+
+    try:        
+        os.mkdir(os.path.join(args.output_folder,'node_timeseries/node_timeseries'))  
+        print('Creating folder: {}'.format(os.path.join(args.output_folder,'node_timeseries/node_timeseries/')))
+    except OSError:
+        print('folder already exist: {}'.format(os.path.join(args.output_folder,'node_timeseries/node_timeseries/')))
+
+    np.save(os.path.join(args.output_folder,'node_timeseries/node_timeseries/adj_matrix.npy'), A)
+
+    # split train/test and save data
+    
+    skf = StratifiedKFold(n_splits=5,shuffle=True)
+    fold = 1
+    for train_idx, test_idx in skf.split(data, label):
+        train_data = data[train_idx]
+        train_label = label[train_idx]
+        test_data = data[test_idx]
+        test_label = label[test_idx] 
+
+        filename = os.path.join(args.output_folder,'node_timeseries/node_timeseries', 'train_data_'+str(fold)+'.npy')
+        np.save(filename,train_data)
+        filename = os.path.join(args.output_folder,'node_timeseries/node_timeseries','train_label_'+str(fold)+'.npy')
+        np.save(filename,train_label)
+        filename = os.path.join(args.output_folder,'node_timeseries/node_timeseries','test_data_'+str(fold)+'.npy')
+        np.save(filename,test_data)
+        filename = os.path.join(args.output_folder,'node_timeseries/node_timeseries','test_label_'+str(fold)+'.npy')
+        np.save(filename,test_label)
+
+        fold = fold + 1
+  
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Preprocessing script.")
+    parser.add_argument('--filename', required=True, help='Path to the subject ID file (txt or csv).')
+    parser.add_argument('--label', required=True, type=str, help='Label name.')
+    parser.add_argument('--data_path', required=True, help='Base directory of node timeseries data.')
+    parser.add_argument('--output_folder', required=True, help='Directory to save the output data.')
+
+    args = parser.parse_args()
+    main(args)
---
+++ tools/train_node_timeseries.py
@@ -0,0 +1,358 @@
+#
+# Created on Tue Jul 18 2024
+#
+# by Rodrigo M. Cabral-Carvalho
+
+# Adapted from Simon Dahan @SD3004
+#
+# Copyright (c) 2024 MeTrICS Lab
+#
+
+import os
+import argparse
+import yaml
+import sys
+sys.path.append('../')
+sys.path.append('./')
+
+import random
+from datetime import datetime
+
+
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import numpy as np
+
+from scipy.stats import pearsonr
+
+from torch.utils.tensorboard import SummaryWriter
+
+from models.MSG3D.model import Model
+
+
+
+def train(args):
+
+    device = torch.device("cuda:{}".format(args.gpu) if torch.cuda.is_available() else "cpu")
+    #device = torch.device("mps:{}".format(args.gpu) if torch.backends.mps.is_available() else "cpu")
+    print(device)   
+
+    ROI_nodes = args.nodes
+    num_epochs = args.epochs + 1
+    TS = args.TS
+
+    batch_size_training = args.bs 
+    batch_size_testing = 32
+    dropout = args.dropout
+
+    num_scales_gcn = args.gcn_scales
+    num_scales_g3d = args.g3d_scales
+
+    LR = args.LR
+
+    W_list = args.windows
+
+    data_path = os.path.join(args.data_path,'node_timeseries/node_timeseries')
+
+
+    hparams = {}
+    hparams['bs'] = args.bs
+    hparams['nodes'] = args.nodes
+    hparams['epochs'] = args.epochs + 1
+    hparams['windows'] = args.windows
+    hparams['windows'] = args.TS
+    hparams['g3d_scales'] = args.gcn_scales
+    hparams['gcn_scales'] = args.g3d_scales
+    hparams['dropout'] = args.dropout
+    hparams['LR'] = args.LR
+    hparams['optim'] = args.optim
+
+    
+
+    criterion = nn.BCELoss()
+
+    print('')
+    print('#'*30)
+    print('Logging training information')
+    print('#'*30)
+    print('')
+
+
+
+
+    for ws in W_list:
+        
+        testing_fold = []
+        
+        ##############################
+        ######      LOGGING     ######
+        ##############################
+
+        # creating folders for logging. 
+
+        folder_to_save_model = './logs/MS-G3D/'
+        os.makedirs(folder_to_save_model, exist_ok=True)
+        print('Creating folder: {}'.format(folder_to_save_model))
+
+        # folder ICAs
+        folder_to_save_model = os.path.join(folder_to_save_model, 'ROI_{}'.format(ROI_nodes))
+        os.makedirs(folder_to_save_model, exist_ok=True)
+        print('Creating folder: {}'.format(folder_to_save_model))
+        
+        # folder ws
+        folder_to_save_model = os.path.join(folder_to_save_model, 'ws_{}'.format(ws))
+        os.makedirs(folder_to_save_model, exist_ok=True)
+        print('Creating folder: {}'.format(folder_to_save_model))
+
+        date = datetime.today().strftime('%Y-%m-%d-%H-%M-%S')
+
+        # folder time
+        folder_to_save_model = os.path.join(folder_to_save_model, date)
+        os.makedirs(folder_to_save_model, exist_ok=True)
+        print('Creating folder: {}'.format(folder_to_save_model))
+
+        # saving hparams
+        with open(os.path.join(folder_to_save_model, 'hparams.yaml'), 'w') as file:
+            yaml.dump(hparams, file)
+
+        # starting tensorboard logging
+        writer = SummaryWriter(log_dir=folder_to_save_model)
+
+
+        ##############################
+        ######     TRAINING     ######
+        ##############################
+
+        print('')
+        print('#'*30)
+        print('Starting training')
+        print('#'*30)
+        print('')
+
+        print('-'*80)
+        print("Window Size {}".format(ws))
+        print('-'*80)
+        for fold in range(1, 6):
+            print('-'*80)
+            print("Window Size {}, Fold {}".format(ws, fold))
+            print('-'*80)
+            
+            best_test_acc_curr_fold = 0
+            best_test_epoch_curr_fold = 0
+            best_test_corr_curr_fold =0
+            
+            train_data = np.load(os.path.join(data_path,'train_data_'+str(fold)+'.npy'))
+            train_data = train_data.reshape(train_data.shape[0], 1, train_data.shape[1], train_data.shape[2], 1)
+            train_label = np.load(os.path.join(data_path,'train_label_'+str(fold)+'.npy'))
+            test_data = np.load(os.path.join(data_path,'test_data_'+str(fold)+'.npy'))
+            test_data = test_data.reshape(test_data.shape[0], 1, test_data.shape[1], test_data.shape[2], 1)
+
+            test_label = np.load(os.path.join(data_path,'test_label_'+str(fold)+'.npy'))
+
+            print(train_data.shape)
+            
+            net = Model(num_class = 1,
+                    num_nodes = ROI_nodes,
+                    num_person = 1,
+                    num_gcn_scales = num_scales_gcn,
+                    num_g3d_scales = num_scales_g3d,
+                    path_to_data = data_path,
+                    dropout= dropout)
+        
+            net.to(device)
+
+            if args.optim == 'SGD':
+                optimizer = optim.SGD(net.parameters(), lr=LR, momentum=0.9)
+            else:
+                optimizer = optim.Adam(net.parameters(), lr=LR, weight_decay=0.001)
+
+            
+
+            for epoch in range(num_epochs):  # number of mini-batches
+                # select a random sub-set of subjects
+                idx_batch = np.random.permutation(int(train_data.shape[0]))
+                idx_batch = idx_batch[:int(batch_size_training)]
+        
+                # construct a mini-batch by sampling a window W for each subject
+                train_data_batch = np.zeros((batch_size_training, 1, ws, ROI_nodes, 1))
+                train_label_batch = train_label[idx_batch]
+
+                for i in range(batch_size_training):
+                    r1 = random.randint(0, train_data.shape[2] - ws)
+                    train_data_batch[i] = train_data[idx_batch[i], :, r1:r1 + ws, :, :]
+
+                train_data_batch_dev = torch.from_numpy(train_data_batch).float().to(device)
+                train_label_batch_dev = torch.from_numpy(train_label_batch).float().to(device)
+
+                # forward + backward + optimize
+                optimizer.zero_grad()
+
+                outputs = net(train_data_batch_dev)
+
+                outputs = torch.sigmoid(outputs)
+                loss = criterion(outputs.squeeze(), train_label_batch_dev)
+                writer.add_scalar('loss/train_{}'.format(fold), loss.item(), epoch+1)
+                outputs = outputs.data.cpu().numpy() > 0.5
+                train_acc = sum(outputs[:, 0] == train_label_batch) / train_label_batch.shape[0]
+                writer.add_scalar('accuracy/train_{}'.format(fold), train_acc, epoch+1)
+
+                loss.backward()
+                optimizer.step()
+
+                ##############################
+                ######    VALIDATION    ######
+                ##############################
+
+
+                # validate on test subjects by voting
+                epoch_val = args.epochs_val
+                if epoch % epoch_val == 0:  # print every K mini-batches
+                    idx_batch = np.random.permutation(int(test_data.shape[0]))
+                    idx_batch = idx_batch[:int(batch_size_testing)]
+
+                    test_label_batch = test_label[idx_batch]
+                    prediction = np.zeros((test_data.shape[0],))
+                    voter = np.zeros((test_data.shape[0],))
+                    # if args.fluid:
+                    #     losses = 0
+                    for v in range(TS):
+                        idx = np.random.permutation(int(test_data.shape[0]))
+
+                        # testing also performed batch by batch (otherwise it produces error)
+                        for k in range(int(test_data.shape[0] / batch_size_testing)):
+                            idx_batch = idx[int(batch_size_testing * k):int(batch_size_testing * (k + 1))]
+
+                            # construct random sub-sequences from a batch of test subjects
+                            test_data_batch = np.zeros((batch_size_testing, 1, ws, ROI_nodes, 1))
+                            for i in range(batch_size_testing):
+                                r1 = random.randint(0, test_data.shape[2] - ws)
+                                test_data_batch[i] = test_data[idx_batch[i], :, r1:r1 + ws, :, :]
+
+                            test_data_batch_dev = torch.from_numpy(test_data_batch).float().to(device)
+                        
+                            outputs = net(test_data_batch_dev)
+                            outputs = outputs.data.cpu().numpy()
+
+                            prediction[idx_batch] = prediction[idx_batch] + outputs[:, 0]
+                            voter[idx_batch] = voter[idx_batch] + 1
+
+                    prediction = prediction / voter
+
+                    test_acc = sum((prediction > 0.5) == test_label) / test_label.shape[0]
+                    print(sum((prediction > 0.5) == test_label) / test_label.shape[0])
+                    print('[%d] testing batch acc %f' % (epoch + 1, test_acc))
+                    writer.add_scalar('accuracy/test_{}'.format(fold), test_acc, epoch+1)
+                    if test_acc > best_test_acc_curr_fold:
+                        best_test_acc_curr_fold = test_acc
+                        best_test_epoch_curr_fold = epoch
+                        print('saving model')
+                        torch.save(net.state_dict(), os.path.join(folder_to_save_model,'checkpoint.pth'))
+
+
+            print("Best accuracy for window {} and fold {} = {} at epoch = {}".format(ws, fold, best_test_acc_curr_fold, best_test_epoch_curr_fold))
+            testing_fold.append(best_test_acc_curr_fold)
+            writer.add_scalar('accuracy_best/test'.format(fold), best_test_acc_curr_fold, fold)
+        
+
+
+if __name__ == '__main__':
+
+    # Set up argument parser
+        
+    parser = argparse.ArgumentParser(description='MS-G3D')
+
+    parser.add_argument(
+                        '--nodes',
+                        type=int,
+                        required=True,
+                        help='the number of ICA nodes')
+
+    parser.add_argument(
+                        '--bs',
+                        type=int,
+                        required=True,
+                        help='batch size training')
+
+    parser.add_argument(
+                        '--epochs',
+                        type=int,
+                        required=True,
+                        help='number of iterations')
+
+    parser.add_argument(
+                        '--gpu',
+                        type=int,
+                        required=True,
+                        help='gpu to use')
+
+    parser.add_argument(
+                        '--data_path',
+                        type=str,
+                        required=True,
+                        default='',
+                        help='path where the data is stored')
+                        
+    parser.add_argument(
+                        '--windows',
+                        type=int,
+                        required=True,
+                        nargs='+',
+                        help='windows')
+
+    parser.add_argument(
+                        '--dropout',
+                        type=float,
+                        required=False,
+                        default=0.0,
+                        help='windows')
+
+                            
+    parser.add_argument(
+                        '--gcn_scales',
+                        type=int,
+                        required=False,
+                        default=8,
+                        help='windows')
+
+                            
+    parser.add_argument(
+                        '--g3d_scales',
+                        type=int,
+                        required=False,
+                        default=8,
+                        help='windows')
+        
+    parser.add_argument(
+                        '--LR',
+                        type=float,
+                        required=False,
+                        default=0.001,
+                        help='windows')
+
+    parser.add_argument(
+                        '--optim',
+                        type=str,
+                        required=False,
+                        default='Adam',
+                        help='windows')
+    
+    parser.add_argument(
+                        '--TS',
+                        type=int,
+                        required=False,
+                        default=64,
+                        help='number of voters per test subject')
+
+    parser.add_argument(
+                        '--epochs_val',
+                        type=int,
+                        required=False, 
+                        default=100,
+                        help='number of iterations')
+
+     
+    args = parser.parse_args()
+    # Call training
+    train(args)
